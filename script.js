61ca8da99865a7e35a7cff05445465f026bea611
Arjuniest <thedivinetreee@gmail.com>
2022-05-19 20:23:09 +0530

    Create button elements and added text content

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const container = document.querySelector('.container');
+const options = document.querySelector('.options');
 
+//options for the number of squares per side for the new grid.
+
+const option1 = document.createElement('div');
+option1.classList.add('button');
+options.appendChild(option1);
+option1.textContent = "small"
+
+const option2 = document.createElement('div');
+option2.classList.add('button');
+options.appendChild(option2);
+option2.textContent = "Medium";
+
+const option3 = document.createElement('div');
+option3.classList.add('button');
+options.appendChild(option3);
+option3.textContent = "LARGE";
 //creating the grids :
 
 function addGrids(){
